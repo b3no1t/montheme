a0420199e185af46306de8dfb46fe02044235692
Benoit <workaholiks@gmail.com>
2024-06-27 11:01:22 +0200

    caroussel

--- theme/category-music.php
+++ theme/category-music.php
@@ -11,5 +11,24 @@ $timber_post     = Timber::get_post();
 $context['post'] = $timber_post;
 // On ajoute le menu
 $context['menu'] = Timber::get_menu('menu_primary');
+//!QUERY---------------
+$context['musics'] = Timber::get_posts([
+    'post_type' => 'music'
+]);
+
+$context['music_big_item'] = Timber::get_posts([
+    'post_type' => 'music',
+    'posts_per_page'=> 1,
+    'order'=>'DESC'
+]);
+$context['music_small_item'] = Timber::get_posts([
+    'post_type' => 'music',
+    'posts_per_page'=> 3,
+    'order'=>'DESC',
+    'offset' => 1
+]);
+
+
+
 // On choisi la vue
 Timber::render( 'category-music.twig', $context );
